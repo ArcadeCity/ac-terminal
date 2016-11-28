cde85b0d144eeb3ff0dc6c4a21f7d0018fe68c9a
Stefaan Ponnet <stefaan@ponnet.com>
2016-11-28 15:45:20 +0100

    first stage in redoing the router / lazy loader.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -22,7 +22,7 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   // Grab a reference to our auto-binding template
   // and give it some initial binding values
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
-  var stage = document.querySelector('#stage');
+  // var stage = document.querySelector('#stage');
 
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page
@@ -36,49 +36,49 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
     // imports are loaded and elements have been registered
   });
 
-  function importPage(url){
-    return new Promise(function(resolve, reject){
-      Polymer.Base.importHref(url, function(e){
-        resolve(e.target.import);
-      }, reject);
-    });
-  }
+  // function importPage(url){
+  //   return new Promise(function(resolve, reject){
+  //     Polymer.Base.importHref(url, function(e){
+  //       resolve(e.target.import);
+  //     }, reject);
+  //   });
+  // }
 
-  stage.generate = function(){
-    console.log('New user!');
-    importPage('/src/ac-newuser.html').then(function(){
-      var element = document.createElement('ac-newuser');
-      element.id = 'acnewuser';
-      document.body.appendChild(element);
-    }, function(err){
-      console.log(err, 'error');
-    });
-  };
+  // stage.generate = function(){
+  //   console.log('New user!');
+  //   importPage('/src/ac-newuser.html').then(function(){
+  //     var element = document.createElement('ac-newuser');
+  //     element.id = 'acnewuser';
+  //     document.body.appendChild(element);
+  //   }, function(err){
+  //     console.log(err, 'error');
+  //   });
+  // };
 
-  stage.homestate = function(){
-    //debugger;
-    console.log('Existing user');
-    if(sessionStorage.getItem('ac-password')){
-      console.log('Password found.');
-      importPage('/src/ac-home.html').then(function(){
-        var element = document.createElement('ac-home');
-        element.id = 'app';
-        document.body.appendChild(element);
-      }, function(err){
-        console.log(err, 'error');
-      });
-    } else {
-      importPage('/src/ac-password.html').then(function(){
-        console.log('Password not found.');
-        var element = document.createElement('ac-password');
-        element.id = 'password';
-        //var app = document.querySelector('#password');
-        document.body.appendChild(element);
-      }, function(err){
-        console.log(err, 'error');
-      });
-    }
-  };
+  // stage.homestate = function(){
+  //   //debugger;
+  //   console.log('Existing user');
+  //   if(sessionStorage.getItem('ac-password')){
+  //     console.log('Password found.');
+  //     importPage('/src/ac-home.html').then(function(){
+  //       var element = document.createElement('ac-home');
+  //       element.id = 'app';
+  //       document.body.appendChild(element);
+  //     }, function(err){
+  //       console.log(err, 'error');
+  //     });
+  //   } else {
+  //     importPage('/src/ac-password.html').then(function(){
+  //       console.log('Password not found.');
+  //       var element = document.createElement('ac-password');
+  //       element.id = 'password';
+  //       //var app = document.querySelector('#password');
+  //       document.body.appendChild(element);
+  //     }, function(err){
+  //       console.log(err, 'error');
+  //     });
+  //   }
+  // };
 
 
 })(document);
